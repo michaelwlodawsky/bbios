6b976ab8f7f01dd2e8f0996161fbd8fed9cb1acf
Michael Wlodawsky <michaelwlodawsky@Michaels-MacBook-Pro.local>
2020-03-31 01:17:39 -0500

    fuck this, we're storyboarding. Quick MVPs to the max bitches

--- bbios/ViewController.swift
+++ bbios/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
+    
+    var email: UITextField!
+    var verifyEmail: UITextField!
+    var password: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        // Initialize Text Fields
+        email = UITextField()
+        email.textAlignment = .center
+        email.placeholder = "example@email.com"
+        verifyEmail = UITextField()
+        verifyEmail.placeholder = "example@email.com"
+        password = UITextField()
+        password.placeholder = "password"
+        // Hide password input contents
+//        password.textContentType = .password
+        let signupButton = UIButton(frame: CGRect(x: 100, y: 40, width: 100, height: 50))
+        signupButton.isEnabled = true
+        signupButton.setTitle("signup", for: .normal)
+        signupButton.setTitleColor(.black, for: .normal)
+        signupButton.addTarget(self, action: #selector(onSignup), for: .touchUpInside)
+        self.view.addSubview(signupButton)
+        //setup UI
+        setupSignupUI(fields: [email, verifyEmail, password])
 
+    }
+    
+    func setupSignupUI(fields: [UITextField]) {
+        var yStart = self.view.frame.height * 0.25
+        for (index, field) in fields.enumerated() {
+            field.center.y = yStart
+            field.center.x = self.view.center.x
+            field.frame.size.height = 50
+            field.frame.size.width = self.view.frame.width * 0.5
+            self.view.addSubview(field)
+            //iterate
+            yStart += 75
+        }
+    }
+    
+    @objc func onSignup(sender: UIButton) {
+        Auth.auth().createUser(withEmail: email.text!, password: password.text!, completion: nil)
+    }
 
 }
 
